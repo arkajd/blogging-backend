792119c59cde8a3819f58215f362b5dfc528a712
Arka Das <arkajd95@gmail.com>
2024-05-11 10:51:45 +0530

    added Blog object

---
+++ src/main/java/com/example/bloggingbackend/models/Blog.java
@@ -0,0 +1,11 @@
+package com.example.bloggingbackend.models;
+
+public class Blog {
+
+    private String blogId;
+    private String title;
+    private String content;
+    private String date;
+    private String time;
+    private User user;
+}
--- src/main/java/com/example/bloggingbackend/models/User.java
+++ src/main/java/com/example/bloggingbackend/models/User.java
@@ -2,6 +2,7 @@ package com.example.bloggingbackend.models;
 
 public class User {
 
+    private String userId;
     private String username;
     private String password;
 }
